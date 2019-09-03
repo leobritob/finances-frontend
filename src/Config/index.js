ca3b50ffd747ceecbe16ec9fee6a5cd4ab89ea9f
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2019-09-02 21:09:45 -0300

    config changes

--- src/Config/index.js
+++ src/Config/index.js
@@ -3,5 +3,5 @@ const DEBUG = process.env.NODE_ENV === 'development';
 export default {
   DEBUG,
   APP_NAME: 'FinancesApp',
-  API_BASE_URL: DEBUG ? 'http://localhost:3333/api' : 'https://finances-app-backend.herokuapp.com'
+  API_BASE_URL: DEBUG ? 'http://localhost:3333/api' : 'https://finances-app-backend.herokuapp.com/api'
 };
