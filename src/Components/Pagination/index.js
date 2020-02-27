3959c0aece1b80d3b6b3bf46470189c685231b1b
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2020-02-26 21:58:37 -0300

    feat(pagination): improve the pagination component with useCallback and useMemo

--- src/Components/Pagination/index.js
+++ src/Components/Pagination/index.js
@@ -1,43 +1,48 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { Container, List, Item, ItemLink } from './styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function doPagination(current, total) {
-  var list = [];
-  var pageLimit = 5;
-  var upperLimit, lowerLimit;
-  var currentPage = (lowerLimit = upperLimit = Math.min(current, total));
-
-  for (var b = 1; b < pageLimit && b < total; ) {
-    if (lowerLimit > 1) {
-      lowerLimit--;
-      b++;
-    }
-    if (b < pageLimit && upperLimit < total) {
-      upperLimit++;
-      b++;
-    }
-  }
-
-  for (var i = lowerLimit; i <= upperLimit; i++) {
-    list.push({
-      isCurrent: i === currentPage,
-      number: i
-    });
-  }
-  return list;
-}
-
 export default function Pagination({ page, perPage, total, paginationOnChange }) {
-  const numberOfPages = Math.ceil(total / perPage);
-  const pagesComponentsList = doPagination(page, numberOfPages).map((item, index) => (
-    <Item key={index}>
-      <ItemLink isCurrent={item.isCurrent} onClick={() => paginationOnChange(item.number)}>
-        {item.number}
-      </ItemLink>
-    </Item>
-  ));
+  const doPagination = useCallback((current, total) => {
+    var list = [];
+    var pageLimit = 5;
+    var upperLimit, lowerLimit;
+    var currentPage = (lowerLimit = upperLimit = Math.min(current, total));
+
+    for (var b = 1; b < pageLimit && b < total; ) {
+      if (lowerLimit > 1) {
+        lowerLimit--;
+        b++;
+      }
+      if (b < pageLimit && upperLimit < total) {
+        upperLimit++;
+        b++;
+      }
+    }
+
+    for (var i = lowerLimit; i <= upperLimit; i++) {
+      list.push({
+        isCurrent: i === currentPage,
+        number: i,
+      });
+    }
+    return list;
+  }, []);
+
+  const numberOfPages = useMemo(() => Math.ceil(total / perPage), [total, perPage]);
+
+  const pagesComponentsList = useMemo(
+    () =>
+      doPagination(page, numberOfPages).map((item, index) => (
+        <Item key={index}>
+          <ItemLink isCurrent={item.isCurrent} onClick={() => paginationOnChange(item.number)}>
+            {item.number}
+          </ItemLink>
+        </Item>
+      )),
+    [doPagination, page, paginationOnChange, numberOfPages]
+  );
 
   return (
     <Container>
@@ -58,11 +63,12 @@ export default function Pagination({ page, perPage, total, paginationOnChange })
   );
 }
 
-Pagination.prototype = {
+Pagination.propTypes = {
   page: PropTypes.number,
   perPage: PropTypes.number,
   total: PropTypes.number,
   goToPage: PropTypes.func,
   prevOnClick: PropTypes.func,
-  nextOnClick: PropTypes.func
+  nextOnClick: PropTypes.func,
+  paginationOnChange: PropTypes.func,
 };
